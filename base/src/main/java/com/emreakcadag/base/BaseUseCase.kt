a2af2b15a94a6ac8120948da6bfd41809268c4ef
Emre Akçadağ <mail@emreakcadag.com>
2022-03-03 23:36:32 +0300

    [ DEV ] [ emre.akcadag ] [ develop ] [ fix. ]

--- base/src/main/java/com/emreakcadag/base/BaseUseCase.kt
+++ base/src/main/java/com/emreakcadag/base/BaseUseCase.kt
@@ -1,6 +1,6 @@
 package com.emreakcadag.base
 
-import com.example.extension.logDebug
+import com.emreakcadag.extension.logDebug
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -12,14 +12,14 @@ abstract class BaseUseCase<in Param, out Result>(
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
 
-    suspend operator fun invoke(params: Param) = runCatching {
+    suspend operator fun invoke(param: Param? = null) = runCatching {
         withContext(dispatcher) {
-            execute(params)
+            execute(param)
         }
     }.getOrElse {
         logDebug(it.message, isError = true)
         it
     }
 
-    protected abstract suspend fun execute(params: Param): Result
+    protected abstract suspend fun execute(param: Param?): Result
 }
