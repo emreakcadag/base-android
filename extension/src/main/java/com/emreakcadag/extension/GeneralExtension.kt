562eeef0f340c340b2d544701a5261a30ce141dd
Emre Akçadağ <mail@emreakcadag.com>
2022-03-03 23:37:01 +0300

    [ DEV ] [ emre.akcadag ] [ develop ] [ Add some extension. ]

--- extension/src/main/java/com/emreakcadag/extension/GeneralExtension.kt
+++ extension/src/main/java/com/emreakcadag/extension/GeneralExtension.kt
@@ -1,4 +1,4 @@
-package com.example.extension
+package com.emreakcadag.extension
 
 import android.util.Log
 import com.emreakcadag.libs.BuildConfig
@@ -6,6 +6,8 @@ import com.emreakcadag.libs.BuildConfig
 /**
  * Created by Emre Akçadağ on 28.02.2022
  */
+
+const val KEY_TRY_CATCH = "TRY_CATCH_"
 
 inline val <reified T> T.TAG: String get() = T::class.java.name
 
@@ -20,4 +22,24 @@ inline fun <reified T> T.logDebug(message: Any?, logTag: String = "LogDebug: ${t
             Log.e(logTag, "${e.message}")
         }
     }
+}
+
+/**
+ * try - catch - finally block extension function.
+ * @returns the given function in tryBlock or catchBlock or null.
+ */
+inline fun <reified C, reified T> C.tryCatch(
+    tryBlock: () -> T?,
+    noinline catchBlock: ((Exception) -> Any?)? = null,
+    noinline finallyBlock: (() -> Unit)? = null
+): T? = try {
+    tryBlock()?.takeUnless { it is Unit }
+
+} catch (e: Exception) {
+    logDebug(e.message, "$KEY_TRY_CATCH${C::class.simpleName}", isError = true)
+    catchBlock?.invoke(e)?.run {
+        takeIf { it is T } as? T?
+    }
+} finally {
+    finallyBlock?.let { it() }
 }
